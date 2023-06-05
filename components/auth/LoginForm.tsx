10c6f928505050b20e8399473721f8f932c53b14
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-06-05 20:17:31 +0300

    add: login form logic

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,6 +1,60 @@
 import React from "react";
 import styles from "./Auth.module.scss";
+import { Button, Form, Input, notification } from "antd";
+import { LoginFormDTO } from "@/api/dto/auth.dto";
+
+import * as Api from "@/api";
+import { setCookie } from "nookies";
 
 export const LoginForm: React.FC = () => {
-  return <div className={styles.formBlock}>Login Form</div>;
+  const onSubmit = async (values: LoginFormDTO) => {
+    try {
+      const { token } = await Api.auth.login(values);
+
+      notification.success({
+        message: "Success!",
+        description: "Moving to admin dashboard..",
+        duration: 2,
+      });
+
+      setCookie(null, "_token", token, {
+        path: "/",
+      });
+    } catch (error) {
+      console.warn("LoginForm", error);
+
+      notification.error({
+        message: "Error(",
+        description: "Wrong login or password",
+        duration: 2,
+      });
+    }
+  };
+  return (
+    <div className={styles.formBlock}>
+      <Form name="basic" labelCol={{ span: 8 }} onFinish={onSubmit}>
+        <Form.Item
+          label="E-Mail"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
--- pages/dashboard/auth.tsx
+++ pages/dashboard/auth.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { LoginForm } from "@/components/auth/LoginForm";
+import { Tabs } from "antd";
 
 const AuthPage: NextPage = () => {
   return (
@@ -9,7 +10,16 @@ const AuthPage: NextPage = () => {
         <title>Dashboard / Auth</title>
       </Head>
       <main style={{ width: 400, margin: "50px auto" }}>
-        <LoginForm />
+        <Tabs
+          items={[
+            { label: "Login", key: "1", children: <LoginForm /> },
+            {
+              label: "Registration",
+              key: "2",
+              children: <h1>Registration</h1>,
+            },
+          ]}
+        />
       </main>
     </>
   );
